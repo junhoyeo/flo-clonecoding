702cc7700a5d9e13bb7a791ef01694875c79ad60
Woo-sang yun <wsy04018@naver.com>
2020-01-13 22:50:59 +0900

    create Navbar (2)
    create search form

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,51 +1,79 @@
 import React, { Fragment } from "react";
 import styled from "styled-components";
 
-const Bar = styled.div`
-  background-color: #ffffff;
-  height: 95px;
-  border: 1px;
-  text-align: center;
-`;
+export default function Navbar() {
+  const Bar = styled.div`
+    background-color: #ffffff;
+    height: 95px;
+    border: 1px;
+    text-align: center;
+  `;
 
-const Logo = styled.img.attrs({
-  src: require("../assets/logo.png")
-})`
-  width: 54.5px;
-  height: auto;
-  margin-right: 25px;
-  vertical-align: -40%;
-`;
+  const Logo = styled.img.attrs({
+    src: require("../assets/logo.png")
+  })`
+    width: 54.5px;
+    height: auto;
+    margin-right: 25px;
+    vertical-align: -40%;
+  `;
 
-const LeftElement = styled.a`
-  color: #000000;
-  text-decoration: none;
-  font-size: 16px;
-  font-weight: 400;
-  margin: 14px;
-  &:active {
+  const LeftElement = styled.a`
     color: #000000;
-  }
-`;
+    text-decoration: none;
+    font-size: 16px;
+    font-weight: 400;
+    margin: 14px;
+  `;
 
-const Leftcontents = styled.span`
-  float: left;
-  margin-top: 43px;
-  margin-left: 12.2%;
-`;
+  const Leftcontents = styled.span`
+    float: left;
+    margin-top: 43px;
+    margin-left: 12.2%;
+  `;
 
-const Navbar = () => {
+  const Searchbox = styled.span`
+    margin-left: 22px;
+    border: 1px solid #cccccc;
+    padding: 7px;
+    border-radius: 100px;
+  `;
+
+  const Searchicon = styled.img.attrs({
+    src: require("../assets/searchicon.png")
+  })`
+    vertical-align: middle;
+    margin: 0 7px 0 5px;
+  `;
+
+  const Searchinput = styled.input.attrs({
+    placeholder: "검색어를 입력하세요."
+  })`
+    vertical-align: middle;
+    border: none;
+    font-size: 13px;
+    font-weight: 400;
+    width: 280px;
+    margin-right: 25px;
+    font-family: "NBG", sans-serif;
+  `;
+
   return (
     <Fragment>
       <Bar>
         <Leftcontents>
-          <Logo />
+          <a href=".">
+            <Logo />
+          </a>
           <LeftElement href=".">둘러보기</LeftElement>
           <LeftElement href=".">보관함</LeftElement>
           <LeftElement href=".">이용권</LeftElement>
+          <Searchbox>
+            <Searchicon />
+            <Searchinput />
+          </Searchbox>
         </Leftcontents>
       </Bar>
     </Fragment>
   );
-};
-export default Navbar;
+}
